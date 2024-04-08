e1215f738684621b2a4a63b910d6da6031b4b1c0
Wojtyua <wojciechderlikiewicz@gmail.com>
2024-04-08 20:46:07 +0200

    test v2

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
       <h1 className="text-3xl text-pink-400">calorie calc :D</h1>
       <h2>siema sprawdzam czy dziala commit ode mn</h2>
       <h1>Siema, tez sprawdzam commit</h1>
+      <p>wojtek jest kozakiem</p>
     </>
   );
 }
