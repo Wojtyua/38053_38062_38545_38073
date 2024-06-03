c089083d7059333f1a4a78ff0903ea1ee70a8858
Wojtyua <wojciechderlikiewicz@gmail.com>
2024-06-03 19:24:02 +0200

    changed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-scroll";
 
 const Navbar = () => {
   return (
-    <div className="px-12 py-4 w-full fixed flex justify-between items-center">
-      <span className="text-white text-xl font-bold">Calorie Calculator</span>
-      <ul className="flex gap-10">
-        <li className="bg-gray-900 text-gray-50 font-bold py-2 px-6 rounded-full border-4 border-emerald-600">
+    <div className="flex justify-between items-center h-12">
+      <span className="text-text text-xl font-bold">Calorie Calculator</span>
+      <ul className="flex space-x-8 text-text font-semibold">
+        <li>
           <Link
             to="landing-page"
             spy={true}
@@ -16,7 +16,7 @@ const Navbar = () => {
             Home
           </Link>
         </li>
-        <li className="bg-gray-900 text-gray-50 font-bold py-2 px-6 rounded-full border-4 border-emerald-600">
+        <li>
           <Link
             to="calculator-section"
             spy={true}
