68ea559804e3787166183d7b00fb4fff5a00a48a
Wojtyua <wojciechderlikiewicz@gmail.com>
2024-06-03 19:12:35 +0200

    removed useless imports

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import './landingPage.css';
-import ChangingText from './ChangingText';
+import "./landingPage.css";
+import ChangingText from "./ChangingText";
 
 const LandingPage = () => {
-  const changingWords = ['kalorie!', 'zdrowie!'];
+  const changingWords = ["kalorie!", "zdrowie!"];
 
   return (
-    <div className="h-screen w-full bg-pink-50 grid grid-cols-2 items-center bg-image-1 top-0 left-0 bg-cover bg-center bg-no-repeat" id="landing-page">
-      <div className='animated-text-container'>
-        <h1 className='animated-text'>Licz swoje <br/> <ChangingText words={changingWords} /></h1>
+    <div
+      className="h-screen w-full bg-pink-50 grid grid-cols-2 items-center bg-image-1 top-0 left-0 bg-cover bg-center bg-no-repeat"
+      id="landing-page"
+    >
+      <div className="animated-text-container">
+        <h1 className="animated-text">
+          Licz swoje <br /> <ChangingText words={changingWords} />
+        </h1>
       </div>
       <div>
         <h2>Policz swoje kalorie</h2>
