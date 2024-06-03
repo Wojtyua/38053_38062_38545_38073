09507d75a6215002b3f54b36a7d565feec0409f1
Wojtyua <wojciechderlikiewicz@gmail.com>
2024-06-03 18:52:21 +0200

    added test message

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef } from 'react';
-import Typed from 'typed.js';
-import './landingPage.css';
+import React, { useEffect, useRef } from "react";
+import Typed from "typed.js";
+import "./landingPage.css";
 
 const LandingPage = () => {
-  const changingWords = ['kalorie!', 'zdrowie!'];
+  const changingWords = ["kalorie!", "zdrowie!"];
   const wordIndexRef = useRef(0);
   const typedRef = useRef(null);
 
@@ -21,7 +21,7 @@ const LandingPage = () => {
           wordIndexRef.current++;
         }
         self.start();
-      }
+      },
     };
 
     const typed = new Typed(typedRef.current, options);
@@ -32,12 +32,18 @@ const LandingPage = () => {
   }, []);
 
   return (
-    <div className="h-screen w-full bg-pink-50 grid grid-cols-2 items-center bg-image-1 top-0 left-0 bg-cover bg-center bg-no-repeat" id="landing-page">
-      <div className='animated-text-container'>
-        <h1 className='animated-text'>Licz swoje <br/> {<span ref={typedRef} className='changing-word'></span>}</h1>
+    <div
+      className="h-screen w-full bg-pink-50 grid grid-cols-2 items-center bg-image-1 top-0 left-0 bg-cover bg-center bg-no-repeat"
+      id="landing-page"
+    >
+      <div className="animated-text-container">
+        <h1 className="animated-text">
+          Licz swoje <br />{" "}
+          {<span ref={typedRef} className="changing-word"></span>}
+        </h1>
       </div>
       <div>
-        <h2>Policz swoje kalorie</h2>
+        <h2>Policz swoje kalorie :D</h2>
       </div>
     </div>
   );
