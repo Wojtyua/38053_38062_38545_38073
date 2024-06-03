44ea3241de36a0aeb010ae9fe0108612a3dbc9e1
Wojtyua <wojciechderlikiewicz@gmail.com>
2024-06-03 18:58:01 +0200

    changed color pallete and added to tailwind conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,17 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundImage:{
-      "image-1": "url(./src/assets/Burgir-A1.png)",
-      "image-2": "url(./src/assets/Burgir-A2.png)"
-      }
+      backgroundImage: {
+        "image-1": "url(./src/assets/Burgir-A1.png)",
+        "image-2": "url(./src/assets/Burgir-A2.png)",
+      },
+      colors: {
+        text: "#070906",
+        background: "#f7f9f6",
+        primary: "#7ca064",
+        secondary: "#adcdc0",
+        accent: "#8db9b6",
+      },
     },
   },
   plugins: [],
