83369c9e48aa123f7b6e48babf6322ea1dfd6caa
Wojtyua <wojciechderlikiewicz@gmail.com>
2024-06-03 19:11:48 +0200

    removed useless imports

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-scroll";
-import Button from "./ui/Button";
 
 const Navbar = () => {
   return (
@@ -7,10 +6,26 @@ const Navbar = () => {
       <span className="text-white text-xl font-bold">Calorie Calculator</span>
       <ul className="flex gap-10">
         <li className="bg-gray-900 text-gray-50 font-bold py-2 px-6 rounded-full border-4 border-emerald-600">
-        <Link to="landing-page" spy={true} smooth={true} duration={1000} className="cursor-pointer">Home</Link>
+          <Link
+            to="landing-page"
+            spy={true}
+            smooth={true}
+            duration={1000}
+            className="cursor-pointer"
+          >
+            Home
+          </Link>
         </li>
         <li className="bg-gray-900 text-gray-50 font-bold py-2 px-6 rounded-full border-4 border-emerald-600">
-          <Link to="calculator-section" spy={true} smooth={true} duration={1000} className="cursor-pointer">Calorie calculator</Link>
+          <Link
+            to="calculator-section"
+            spy={true}
+            smooth={true}
+            duration={1000}
+            className="cursor-pointer"
+          >
+            Calorie calculator
+          </Link>
         </li>
       </ul>
     </div>
