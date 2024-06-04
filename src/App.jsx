fbcb4fbbabe12ef5f9cea2ed877161e2cb5fdcc8
Wojtyua <wojciechderlikiewicz@gmail.com>
2024-06-04 14:58:08 +0200

    adjusted layout, changed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="px-24 text-text bg-background">
-      <div className="h-screen">
-        <Navbar />
-        <LandingPage />
+      <div className=" mx-auto max-w-[1800px]">
+        <div className="h-screen">
+          <Navbar />
+          <LandingPage />
+        </div>
+        <CalculatorSection />
       </div>
-      <CalculatorSection />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-scroll";
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center h-12">
-      <span className="text-text text-xl font-bold">Calorie Calculator</span>
-      <ul className="flex space-x-8 text-text font-semibold">
+      <span className="text-text text-2xl font-bold">Calorie Calculator</span>
+      <ul className="flex space-x-12 text-text font-semibold text-lg">
         <li>
           <Link
             to="landing-page"
             spy={true}
             smooth={true}
             duration={1000}
-            className="cursor-pointer"
+            className="cursor-pointer hover:text-primary transition-colors"
           >
             Home
           </Link>
@@ -22,7 +22,7 @@ const Navbar = () => {
             spy={true}
             smooth={true}
             duration={1000}
-            className="cursor-pointer"
+            className="cursor-pointer hover:text-primary transition-colors"
           >
             Calorie calculator
           </Link>
