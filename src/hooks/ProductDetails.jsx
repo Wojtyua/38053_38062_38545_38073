c886b94a31c9020869022c5be3f43c24f25fa6d0
SzostekMateusz <m.szostek00@gmail.com>
2024-06-14 19:43:47 +0200

    added styles to both tables (summary, select product)

--- src/hooks/ProductDetails.jsx
+++ src/hooks/ProductDetails.jsx
@@ -2,31 +2,31 @@ import React from "react";
 
 const ProductDetails = ({ product, onAddProduct }) => {
   return (
-    <div className="mt-4 p-4 text-center">
+    <div className="mt-4 p-4 text-center bg-white shadow-md rounded-lg">
       <h1 className="text-xl capitalize font-bold mb-4">Szczegóły: "{product.name}"</h1>
-      <table className="text-center">
-        <thead>
+      <table className="min-w-full bg-white border border-gray-200">
+        <thead className="bg-primary">
           <tr>
-            <th>Kalorie</th>
-            <th>Białko</th>
-            <th>Węglowodany</th>
-            <th>Cukier</th>
-            <th>Tłuszcz</th>
+            <th className="py-2 px-4 border-b border-gray-200">Kalorie</th>
+            <th className="py-2 px-4 border-b border-gray-200">Białko</th>
+            <th className="py-2 px-4 border-b border-gray-200">Węglowodany</th>
+            <th className="py-2 px-4 border-b border-gray-200">Cukier</th>
+            <th className="py-2 px-4 border-b border-gray-200">Tłuszcz</th>
           </tr>
         </thead>
         <tbody>
-          <tr className="capitalize">
-            <td>{Math.round(product.calories)} kcal</td>
-            <td>{Math.round(product.protein)}g</td>
-            <td>{Math.round(product.carbs)}g</td>
-            <td>{Math.round(product.sugar)}g</td>
-            <td>{Math.round(product.fat)}g</td>
+          <tr className="capitalize bg-white even:bg-gray-50">
+            <td className="py-2 px-4 border-b border-gray-200">{Math.round(product.calories)} kcal</td>
+            <td className="py-2 px-4 border-b border-gray-200">{Math.round(product.protein)}g</td>
+            <td className="py-2 px-4 border-b border-gray-200">{Math.round(product.carbs)}g</td>
+            <td className="py-2 px-4 border-b border-gray-200">{Math.round(product.sugar)}g</td>
+            <td className="py-2 px-4 border-b border-gray-200">{Math.round(product.fat)}g</td>
           </tr>
         </tbody>
       </table>
       <button
         onClick={onAddProduct}
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-full"
+        className="mt-4 px-4 py-2 bg-secondary text-text rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
       >
         Dodaj
       </button>
--- src/hooks/SelectedProductsTable.jsx
+++ src/hooks/SelectedProductsTable.jsx
@@ -2,37 +2,37 @@ import React from "react";
 
 const SelectedProductsTable = ({ selectedProducts, totals }) => {
   return (
-    <div className="mt-4 p-4 text-center">
+    <div className="mt-4 p-4 text-center bg-white shadow-md rounded-lg">
       <h1 className="text-xl font-bold mb-4">Wybrane produkty:</h1>
-      <table className="text-center">
-        <thead>
+      <table className="min-w-full bg-white border border-gray-200">
+        <thead className="bg-primary">
           <tr>
-            <th>Nazwa</th>
-            <th>Kalorie</th>
-            <th>Białko</th>
-            <th>Węglowodany</th>
-            <th>Cukier</th>
-            <th>Tłuszcz</th>
+            <th className="py-2 px-4 border-b border-gray-200">Nazwa</th>
+            <th className="py-2 px-4 border-b border-gray-200">Kalorie</th>
+            <th className="py-2 px-4 border-b border-gray-200">Białko</th>
+            <th className="py-2 px-4 border-b border-gray-200">Węglowodany</th>
+            <th className="py-2 px-4 border-b border-gray-200">Cukier</th>
+            <th className="py-2 px-4 border-b border-gray-200">Tłuszcz</th>
           </tr>
         </thead>
         <tbody>
           {selectedProducts.map((product, index) => (
-            <tr className="capitalize" key={index}>
-              <td>{product.name}</td>
-              <td>{product.calories} kcal</td>
-              <td>{product.protein}g</td>
-              <td>{product.carbs}g</td>
-              <td>{product.sugar}g</td>
-              <td>{product.fat}g</td>
+            <tr className="capitalize bg-white even:bg-gray-50" key={index}>
+              <td className="py-2 px-4 border-b border-gray-200">{product.name}</td>
+              <td className="py-2 px-4 border-b border-gray-200">{Math.round(product.calories)} kcal</td>
+              <td className="py-2 px-4 border-b border-gray-200">{Math.round(product.protein)}g</td>
+              <td className="py-2 px-4 border-b border-gray-200">{Math.round(product.carbs)}g</td>
+              <td className="py-2 px-4 border-b border-gray-200">{Math.round(product.sugar)}g</td>
+              <td className="py-2 px-4 border-b border-gray-200">{Math.round(product.fat)}g</td>
             </tr>
           ))}
-          <tr className="font-bold">
-            <td>Total</td>
-            <td>{totals.calories.toFixed(2)} kcal</td>
-            <td>{totals.protein.toFixed(2)}g</td>
-            <td>{totals.carbs.toFixed(2)}g</td>
-            <td>{totals.sugar.toFixed(2)}g</td>
-            <td>{totals.fat.toFixed(2)}g</td>
+          <tr className="font-bold bg-secondary">
+            <td className="py-2 px-4 border-b border-gray-200">Total</td>
+            <td className="py-2 px-4 border-b border-gray-200">{totals.calories.toFixed(0)} kcal</td>
+            <td className="py-2 px-4 border-b border-gray-200">{totals.protein.toFixed(0)}g</td>
+            <td className="py-2 px-4 border-b border-gray-200">{totals.carbs.toFixed(0)}g</td>
+            <td className="py-2 px-4 border-b border-gray-200">{totals.sugar.toFixed(0)}g</td>
+            <td className="py-2 px-4 border-b border-gray-200">{totals.fat.toFixed(0)}g</td>
           </tr>
         </tbody>
       </table>
